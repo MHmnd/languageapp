9a715d58bf353c45cfbeb3cfdd6bbc1fd0c9ff30
Mhmnd <mycah@slowthecurry.com>
2017-06-30 15:21:17 -1000

    theapp

---
+++ app/src/main/java/com/slowthecurry/mycahh/learning/Language.java
@@ -0,0 +1,19 @@
+package com.slowthecurry.mycahh.learning;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Language extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_language);
+
+    }
+}
---
+++ app/src/main/java/com/slowthecurry/mycahh/learning/MainActivity.java
@@ -0,0 +1,58 @@
+package com.slowthecurry.mycahh.learning;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends BaseActivity {
+    private final String DEBUG_TAG = "MAIN ACTIVITY";
+
+    //Firebabse declarations
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener fbAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        fbAuthStateListener =new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+                if(currentUser != null){
+                    //currentUser is signed in
+                    Log.d(DEBUG_TAG, currentUser.getProviderId());
+                }else{
+                    //Nobody home y'all!
+                    Log.d(DEBUG_TAG, "NOT SIGNED IN YO!!!!");
+                }
+
+            }//end @Override onAuthStateChanged()
+
+        };//end fbAuthStateListener instantiation
+
+    }//end onCreate()
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(fbAuthStateListener);
+    }//end onStart()
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(fbAuthStateListener);
+    }//end onStop()
+
+
+
+    //end MainActivity
+}
